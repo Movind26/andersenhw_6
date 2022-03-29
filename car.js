4549997da68e5c23c678c50d30d4d730e2a570e7
Movind26 <85275381+Movind26@users.noreply.github.com>
2022-03-29 18:52:23 +0300

    Update car.js
    to Single Quote

--- car.js
+++ car.js
@@ -13,7 +13,7 @@ class Car {
     if (value && value.length <= 50) {
       this.#brand = value;
     } else {
-      throw new Error("Invalid value range");
+      throw new Error('Invalid value range');
     }
   }
 
@@ -25,7 +25,7 @@ class Car {
     if (value && value.length <= 50) {
       this.#model = value;
     } else {
-      throw new Error("Invalid value range");
+      throw new Error('Invalid value range');
     }
   }
 
@@ -39,7 +39,7 @@ class Car {
     if (1900 <= value <= currentYear) {
       this.#yearOfManufacturing = value;
     } else {
-      throw new Error("Invalid value range");
+      throw new Error('Invalid value range');
     }
   }
 
@@ -51,7 +51,7 @@ class Car {
     if (100 <= value && value < 300) {
       this.#maxSpeed = value;
     } else {
-      throw new Error("Invalid value range");
+      throw new Error('Invalid value range');
     }
   }
 
@@ -63,7 +63,7 @@ class Car {
     if (5 <= value && value <= 20) {
       this.#maxFuelVolume = value;
     } else {
-      throw new Error("Invalid value range");
+      throw new Error('Invalid value range');
     }
   }
 
@@ -93,7 +93,7 @@ class Car {
 
   start() {
     if (this.#isStarted) {
-      throw new Error("Car is already started");
+      throw new Error('Car is already started');
     } else {
       return (this.#isStarted = true);
     }
@@ -101,20 +101,20 @@ class Car {
 
   shutDownEngine() {
     if (!this.#isStarted) {
-      throw new Error("Car is not started");
+      throw new Error('Car is not started');
     } else {
       return (this.#isStarted = false);
     }
   }
 
   fillUpGasTank(amount) {
-    if (typeof amount !== "number" || amount <= 0) {
-      throw new Error("Invalid amount of fuel");
+    if (typeof amount !== 'number' || amount <= 0) {
+      throw new Error('Invalid amount of fuel');
     } else {
       this.#currentFuelVolume += amount;
 
       if (this.#maxFuelVolume < this.#currentFuelVolume) {
-        throw new Error("Fuel tank is full");
+        throw new Error('Fuel tank is full');
       }
 
       return this.#currentFuelVolume;
@@ -122,16 +122,19 @@ class Car {
   }
 
   drive(speed, hoursOfTime) {
-    if (typeof speed !== "number" || speed <= 0) {
-      throw new Error("Invalid speed");
-    } else if (typeof hoursOfTime !== "number" || hoursOfTime <= 0) {
-      throw new Error("Invalid amount of time");
+    if (typeof speed !== 'number' || speed <= 0) {
+      throw new Error('Invalid speed');
+    } else if (typeof hoursOfTime !== 'number' || hoursOfTime <= 0) {
+      throw new Error('Invalid amount of time');
     } else if (speed > this.#maxSpeed) {
       throw new Error("Car is can't drive much faster");
     } else if (this.#isStarted === false) {
-      throw new Error("Car must be started");
-    } else if ((speed * hoursOfTime) / this.#fuelConsumption > this.#currentFuelVolume) {
-      throw new Error("Insufficient fuel");
+      throw new Error('Car must be started');
+    } else if (
+      (speed * hoursOfTime) / this.#fuelConsumption >
+      this.#currentFuelVolume
+    ) {
+      throw new Error('Insufficient fuel');
     }
 
     this.#currentFuelVolume -= (speed * hoursOfTime) / this.#fuelConsumption;
